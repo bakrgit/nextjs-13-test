d0f9793385654c73891b132f7a1700e2874215bd
iMac <imac@iMacs-iMac.local>
2023-06-09 18:35:29 +0300

    btn

--- app/page.js
+++ app/page.js
@@ -1,12 +1,11 @@
-
 import Link from "next/link";
 
 export default function Home() {
-    return (
-        <main>
-            <h1 className="text-danger">home page</h1>
-            <Link href={"/data"}>Data Page</Link>
-            <button className="btn btn-danger">Click</button>
-        </main>
-    )
+  return (
+    <main>
+      <h1 className="text-danger">home page</h1>
+      <Link href={"/data"}>Data Page</Link>
+      <button className="btn btn-danger">Click Me</button>
+    </main>
+  );
 }

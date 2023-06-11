93f14b602a8b941b5150ff0e87ebfe59f7b9d235
bakrgit <201740593@fa-hists.edu.eg>
2023-06-12 00:23:51 +0300

    pr

--- app/post/page.js
+++ app/post/page.js
@@ -9,7 +9,7 @@ const Post =async () => {
                 Lorem ipsum dolor sit amet consectetur adipisicing elit.
             </p>
 
-<h1>hello change22223333</h1>
+<h1>hello change22223333 ;pull</h1>
             {data && <PostComponent data={data}/>}
 
         </>
